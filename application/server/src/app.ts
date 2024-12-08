09ee100e6694c8bd914c89f07c1863fde7eb5105
Aidan <abayercalvert@gmail.com>
2024-12-08 00:22:44 -0800

    checking secret keys

--- application/server/src/app.ts
+++ application/server/src/app.ts
@@ -24,6 +24,9 @@ dotenv.config();
 
 const app = express();
 
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
+
 // testAddGames()
 //   .then(() => {
 //     console.log('success');
